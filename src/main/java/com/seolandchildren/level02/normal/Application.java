fd582fc2b5e3cb9d0501ce54f6568e11a287674d
phj9745 <117703172+phj9745@users.noreply.github.com>
2023-05-03 21:30:38 +0900

    normal 문제 풀이입니다.

---
+++ src/main/java/com/seolandchildren/level02/normal/Application.java
@@ -0,0 +1,11 @@
+package com.seolandchildren.level02.normal;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(RandomMaker.randomNumber(-40, 100));
+        System.out.println(RandomMaker.randomUpperAlpabet(10));
+        System.out.println(RandomMaker.rockPaperScissors());
+        System.out.println(RandomMaker.tossCoin());
+    }
+}
+m
